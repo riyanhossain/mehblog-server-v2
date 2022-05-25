10ee7dbea09bbdf93a8411c51792ae7664fe4c47
mehrabriyan <mehraabriyan@gmail.com>
2022-05-25 23:40:22 +0600

    response end

--- Routes/blogs.js
+++ Routes/blogs.js
@@ -20,6 +20,7 @@ router.get('/blogs', async (req, res) => {
     try {
         const blogs = await blog.find();
         res.status(200).json(blogs);
+        res.end();
     } catch (err) {
         res.status(500).json({
             error : err.message
