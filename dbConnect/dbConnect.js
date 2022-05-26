4fc948559d41e1bf4b7da0ded869e28d00c672ba
mehrabriyan <mehraabriyan@gmail.com>
2022-05-26 07:52:17 +0600

    db f

--- dbConnect/dbConnect.js
+++ dbConnect/dbConnect.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connect = async() => {
         try{
-            await mongoose.connect(process.env.DB_URL)
+            await mongoose.connect('mongodb+srv://mehrabriyan:Cr6J4QfXRe3rBPn@cluster0.f6ym0.mongodb.net/mehblog?retryWrites=true&w=majority')
             console.log('connected to db');
         }
         catch(err){
